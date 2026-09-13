860e6cd2025eca04f1c9dcf1b50e1c662b0453be
Bhaskar Dutta <bhaskardutta2209@gmail.com>
2026-09-13 23:53:33 +0530

    Chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use sqlx::PgPool;
+use zero2prod::{configuration::get_configuration, startup::run};
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    // Load configuration and panic if failed
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))
+        .expect("Failed to bind to an open port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Listening on http://127.0.0.1:{}", port);
+    run(listener, connection)?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,95 @@
+use sqlx::PgPool;
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+async fn spawn_app(db_pool: PgPool) -> TestApp {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let server =
+        zero2prod::startup::run(listener, db_pool.clone()).expect("Failed to bind address");
+    tokio::spawn(server);
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
+}
+
+#[sqlx::test]
+async fn health_check_works(pool: PgPool) {
+    let test_app = spawn_app(pool).await;
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("{}/health_check", test_app.address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[sqlx::test]
+async fn subscribe_returns_a_200_for_valid_form_data(pool: PgPool) {
+    let test_app = spawn_app(pool.clone()).await;
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", test_app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+}
+
+#[sqlx::test]
+async fn subscribe_returns_a_400_when_data_is_missing(pool: PgPool) {
+    let test_app = spawn_app(pool).await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", test_app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the playload was {}",
+            error_message
+        );
+    }
+}
